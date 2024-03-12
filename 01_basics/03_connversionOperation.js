fd34a29934dfc8681e6405dbd3720748bc4c3939
rajeshchoudhary7762 <rajeshchoudhary7762@gmail.com>
2024-03-12 09:37:14 +0530

    comparison

--- 01_basics/03_connversionOperation.js
+++ 01_basics/03_connversionOperation.js
@@ -2,28 +2,54 @@
 let score = "22";
 let scoreNumber = Number(score);
 
-console.table([typeof scoreNumber, scoreNumber]);
+// console.table([typeof scoreNumber, scoreNumber]);
 
 
 //Number to String
 let scoreString = String(scoreNumber);
 
-console.table([typeof scoreString, scoreString]);
+// console.table([typeof scoreString, scoreString]);
 
 
 //Number to Boolean
 let isLoggedIn = 1;
 let isLoggedInBoolean = Boolean(isLoggedIn);
 
-console.table([typeof isLoggedInBoolean, isLoggedInBoolean]);
+// console.table([typeof isLoggedInBoolean, isLoggedInBoolean]);
 
 
 //Number to Boolean
 isLoggedInBoolean = true;
 let isLoggedInNumber = Number(isLoggedInBoolean);
 
-console.table([typeof isLoggedInNumber, isLoggedInNumber]);
+// console.table([typeof isLoggedInNumber, isLoggedInNumber]);
 /*
 1 => true; 0 => false
 "" => false; "Rajesh" => true;
 */
+
+
+// ***********************[ Operations ]******************************
+
+// Positive to Negative value
+let posValue = 10;
+let negValue = -posValue;
+
+// console.log(negValue);
+
+
+// Negative to Positive value
+posValue = Math.abs(negValue);
+
+// console.log(posValue);
+
+
+// console.log(2+2); // 2+2 = 4
+// console.log(2-2); // 2-2 = 0
+// console.log(2*2); // 2*2 = 4
+// console.log(2**3); // 2*2*2 = 8
+// console.log(2/2); // 2/2 = 1
+// console.log(2%5); 
+// console.log(2%4); 
+// console.log(2%3); 
+console.log(75%6); 
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("2" == 2);
+
+//Check Strictly
+console.log("2" === 2);
